0ca494a4bc87d12b94ecbc4cac4318882b43e29b
Eugenus <skakovsku@yandex.ru>
2022-06-10 17:22:34 +0300

    Create TAG

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,15 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from users.tokens import EmailObtainAuthToken, token_logout
+from .views import TagViewSet
 
 app_name = 'api'
 
 router = DefaultRouter()
-# router.register('user', UserViewSet)
+router.register('tags', TagViewSet)
 
 urlpatterns = [
     path('auth/token/login/', EmailObtainAuthToken.as_view()),
     path('auth/token/logout/', token_logout, name='token_logout'),
     path('users/', include('users.urls')),
+    path('', include(router.urls)),
     path('', include('djoser.urls')),
 ]
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.permissions import AllowAny
+from rest_framework import exceptions
+from recipes.models import Tag
+from recipes.serializers import TagSerializer
 
-# Create your views here.
+
+class TagViewSet(ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    pagination_class = None
+    permission_classes = (AllowAny,)
+
+    def retrieve(self, request, *args, **kwargs):
+        obj = Tag.objects.filter(pk=kwargs['pk'])
+        if len(obj) == 0:
+            raise exceptions.NotFound(
+                {'detail': 'Страница не найдена.'}
+            )
+        return super().retrieve(request, *args, **kwargs)
