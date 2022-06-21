a3784ddefe281a5f82f80073e9e83f88901ef845
Eugenus <skakovsku@yandex.ru>
2022-06-21 20:51:29 +0300

    End API

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -4,15 +4,27 @@ from recipes.models import Recipe, RecipeUsers
 
 
 class RecipeFilter(FilterSet):
-    is_favorite = filter.CharFilter(method='get_is_favorite')
+    is_favorite = filter.NumberFilter(method='get_is_favorite')
+    in_is_shopping_cart = filter.NumberFilter(
+        method='get_in_is_shopping_cart'
+    )
 
     class Meta:
         model = Recipe
-        fields = ['is_favorite']
+        fields = ['is_favorite', 'in_is_shopping_cart']
 
     def get_is_favorite(self, queryset, name, value):
         if value == 1:
             if self.request.user.is_authenticated:
-                obj = RecipeUsers.objects.get(user=self.request.user)
-                queryset = obj.users_favorite.all()
+                obj = RecipeUsers.objects.filter(user=self.request.user)
+                if obj.count() != 0:
+                    queryset = obj[0].users_favorite.all()
+        return queryset
+
+    def get_in_is_shopping_cart(self, queryset, name, value):
+        if value == 1:
+            if self.request.user.is_authenticated:
+                obj = RecipeUsers.objects.filter(user=self.request.user)
+                if obj.count() != 0:
+                    queryset = obj[0].users_shopping.all()
         return queryset
