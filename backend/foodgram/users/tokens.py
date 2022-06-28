e2c8eb228cff4aed0aadf9e04330b13e237fde1e
Eugenus <skakovsku@yandex.ru>
2022-06-28 21:09:09 +0300

    Signin

--- backend/foodgram/users/tokens.py
+++ backend/foodgram/users/tokens.py
@@ -63,7 +63,7 @@ class EmailObtainAuthToken(APIView):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key}, status.HTTP_201_CREATED)
+        return Response({'auth_token': token.key}, status.HTTP_201_CREATED)
 
 
 @api_view(('POST',))
